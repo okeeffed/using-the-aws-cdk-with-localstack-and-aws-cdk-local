c41dffc6aa58dc6fcd52337528422be7b8fd9be9
arjun123970 <arjunmajety07@gmail.com>
2021-11-30 21:37:48 -0500

    Created a Security group for my EC2 instance

--- test/aws-cdk-with-typescript-foundations.test.ts
+++ test/aws-cdk-with-typescript-foundations.test.ts
@@ -16,6 +16,31 @@ export class CdkStarterStack extends cdk.Stack {
       ],
     });
 
+        // 👇 create Security Group for the Instance
+    const webserverSG = new ec2.SecurityGroup(this, 'webserver-sg', {
+      vpc,
+      allowAllOutbound: true,
+    });
+
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'allow SSH access from anywhere',
+    );
+
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'allow HTTP traffic from anywhere',
+    );
+
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      'allow HTTPS traffic from anywhere',
+    );
+
+
     
   }
 }
