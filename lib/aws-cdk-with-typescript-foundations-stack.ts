114d44b25e7da28ac3e4cbc45953654e25c23222
arjun123970 <arjunmajety07@gmail.com>
2021-11-30 21:36:00 -0500

    Created the VPC block of code

--- lib/aws-cdk-with-typescript-foundations-stack.ts
+++ lib/aws-cdk-with-typescript-foundations-stack.ts
@@ -16,55 +16,6 @@ export class CdkStarterStack extends cdk.Stack {
       ],
     });
 
-    // 👇 create Security Group for the Instance
-    const webserverSG = new ec2.SecurityGroup(this, 'webserver-sg', {
-      vpc,
-      allowAllOutbound: true,
-    });
-
-    webserverSG.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(22),
-      'allow SSH access from anywhere',
-    );
-
-    webserverSG.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(80),
-      'allow HTTP traffic from anywhere',
-    );
-
-    webserverSG.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(443),
-      'allow HTTPS traffic from anywhere',
-    );
-
-    // 👇 create a Role for the EC2 Instance
-    const webserverRole = new iam.Role(this, 'webserver-role', {
-      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
-      managedPolicies: [
-        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3ReadOnlyAccess'),]
-      }
-    );
     
-    const ec2Instance = new ec2.Instance(this, 'ec2-instance', {
-      vpc,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PUBLIC,
-      },
-      securityGroup: webserverSG,
-      instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.T2,
-        ec2.InstanceSize.MICRO,
-      ),
-      machineImage: new ec2.AmazonLinuxImage({
-        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-      }),
-      keyName: 'ec2-key-pair', }
-    ); 
-
-    const userDataScript = readFileSync('SourcegraphCode.txt', 'utf8')
-    ec2Instance.addUserData(userDataScript);
   }
 }
